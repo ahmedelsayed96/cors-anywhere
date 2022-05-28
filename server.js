0853473379b67341d23b4126459117b9e44363f2
ahmido <ahmed.elsayed.ali20@gmail.com>
2022-05-28 18:02:49 +0200

    token api

--- server.js
+++ server.js
@@ -20,11 +20,18 @@ function parseEnvList(env) {
 // var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
 
 var cors_proxy = require('./lib/cors-anywhere');
-cors_proxy.createServer({
+const express = require("express");
+var https = require('https');
+const app = express();
+const qs = require('qs');
+const axios = require('axios').default;
+
+const cors_server = cors_proxy.createServer({
   // originBlacklist: originBlacklist,
   originWhitelist: [], // Allow all origins
   // requireHeader: ['origin', 'x-requested-with'],
   // checkRateLimit: checkRateLimit,
+  
   removeHeaders: [
     'cookie',
     'cookie2',
@@ -44,6 +51,45 @@ cors_proxy.createServer({
     // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
     xfwd: false,
   },
-}).listen(port, host, function() {
-  console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
+//   .listen(port, host, function () {
+//   console.log('Running CORS Anywhere on ' + host + ':' + port);
+// });
+
+app.get('/proxy/:proxyUrl*', (req, res) => {
+  req.url = req.url.replace('/proxy/', '/'); // Strip '/proxy' from the front of the URL, else the proxy won't work.
+  cors_server.emit('request', req, res);
+});
+app.post('/proxy/:proxyUrl*', (req, res) => {
+  req.url = req.url.replace('/proxy/', '/'); // Strip '/proxy' from the front of the URL, else the proxy won't work.
+  cors_server.emit('request', req, res);
+});
+app.post('/token', async (req, res) => {
+  const tenantId = 'db17403f-3ed0-46c8-8e97-120639b5a2ea';
+  const body = {
+    grant_type: 'client_credentials',
+    client_id: '027986d1-25fc-41d1-8169-ebc9a45c9ebe',
+    client_secret: '3VA8Q~57Utf3WhZJ0pUbAufHxatj.c9TjfFuydjJ',
+    scope: 'https://kbdenim-uat.sandbox.operations.dynamics.com/.default',
+    
+    
+  };
+  var data = qs.stringify(body);
+
+  try {
+  const response  =await  axios.post('https://login.microsoftonline.com/'+tenantId+'/oauth2/v2.0/token', data ,{ headers: {
+    // Overwrite Axios's automatically set Content-Type
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+  });
+    
+  res.send(response.data);
+
+  } catch (err){
+    res.send( err);
+}
+});
+
+app.listen(port, () => {
+  console.log(`Server start ....... `)
+})
